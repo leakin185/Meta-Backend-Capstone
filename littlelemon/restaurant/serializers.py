fd289765ecc55f6ef9ffc2a9c5b1c70af7a6b61f
wang_lea <wang1345@e.ntu.edu.sg>
2023-05-08 01:27:27 +0800

    set up the Menu API for the Restaurant app

---
+++ littlelemon/restaurant/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from .models import Menu
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'groups']
+
+
+class MenuSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Menu
+        fields = ['title', 'price', 'inventory']
