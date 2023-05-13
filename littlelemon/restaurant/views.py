c916bb30a74b34eb2875c31c366badff55cc850c
wang_lea <wang1345@e.ntu.edu.sg>
2023-05-13 11:46:03 +0800

    added authentication for bookings

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -21,6 +21,7 @@ class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView
     serializer_class = MenuSerializer
 
 class BookingItemsView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
 
