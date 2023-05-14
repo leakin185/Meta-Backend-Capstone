e8540e2109fdc2ba5f7d7909998cc1c8408e6f00
wang_lea <wang1345@e.ntu.edu.sg>
2023-05-14 17:09:55 +0800

    updated tests

--- littlelemon/littlelemon/tests/test_models.py
+++ littlelemon/littlelemon/tests/test_models.py
@@ -10,4 +10,5 @@ from restaurant.models import Menu, Booking
 class MenuItemTest(TestCase):
     def test_get_item(self):
         item = Menu.objects.create(title="IceCream", price=80, inventory=100)
+        item = str(item)
         self.assertEqual(item, "IceCream : 80")
